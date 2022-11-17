9a81ad4938b4048216924d72ccac9b8e1d3f0303
Nguyen Dang Quy <dangquyitt@gmail.com>
2022-11-17 22:29:13 +0700

    create project

---
+++ SpringSecurityTutorials/src/main/java/com/junior/SpringSecurityTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTutorialsApplication.class, args);
+	}
+
+}
