b2c773ecf878f127bd84438802ffcf42526b5d01
Nguyen Dang Quy <dangquyitt@gmail.com>
2022-11-18 13:38:07 +0700

    update login logout

--- SpringSecurityTutorials/src/main/java/com/junior/controller/DashBoardController.java
+++ SpringSecurityTutorials/src/main/java/com/junior/controller/DashBoardController.java
@@ -1,12 +1,16 @@
 package com.junior.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashBoardController {
     @GetMapping(value = "dashboard")
-    public String getDashboard() {
+    public String getDashboard(@AuthenticationPrincipal User userDetails, Model model) {
+        model.addAttribute("userDetails", userDetails);
         return "dashboard";
     }
 }
--- SpringSecurityTutorials/src/main/java/com/junior/controller/LoginController.java
+++ SpringSecurityTutorials/src/main/java/com/junior/controller/LoginController.java
@@ -1,12 +1,18 @@
 package com.junior.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public String login(@AuthenticationPrincipal User userDetails) {
+        if(userDetails == null) {
+            return "login";
+        }
+        return "redirect:/dashboard";
     }
 }
